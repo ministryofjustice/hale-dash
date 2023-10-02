6f68026f4945e9d73a5ac2e20fa3bc0d36beb6aa
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-10-02 12:12:46 +0100

    Add Dashboard Page Template

---
+++ page-dashboard.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Dashboard Page
+ *
+ * @package Hale Dash
+ * @copyright Ministry of Justice
+ * @version 2.0
+ */
+
+get_header();
+
+echo 'Dashboard page';
+
+get_footer();
+
+
+
