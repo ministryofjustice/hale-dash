1274436be2eaeb84b217645baef3ecc81a4439e6
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2023-10-03 08:55:13 +0100

    Add basic site list

--- page-dashboard.php
+++ page-dashboard.php
@@ -9,7 +9,37 @@
 
 get_header();
 
-echo 'Dashboard page';
+$environments = [
+    'prod',
+    'staging',
+    'dev',
+    'demo'
+];
+
+$sites = get_sites();
+foreach ( $sites as $site ) {
+    switch_to_blog( $site->blog_id ); ?>
+
+    <div>
+        <?php echo get_bloginfo('name'); ?>
+
+        <?php
+        foreach ($environments as $env) { 
+            
+            $env_url = "https://hale-platform-" . $env . ".apps.live.cloud-platform.service.justice.gov.uk/"
+            
+            ?>
+            <a href="<?php echo $env_url; ?>" class="govuk-button"><?php echo ucfirst($env);?></a>
+        <?php
+
+        }
+
+        ?>
+    </div>
+    <?php
+    restore_current_blog();
+}
+
 
 get_footer();
 
