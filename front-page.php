294900b75f2f24a6d3aeb6e388e79025f78d5e2a
adam <brown-a2@users.noreply.github.com>
2023-10-12 11:03:17 +0100

    Add key dev metric links to dash

--- front-page.php
+++ front-page.php
@@ -26,16 +26,37 @@ echo festiveGreeting(time());
 echo getBrithday();
 ?>
 <h1 class="govuk-heading-l govuk-grid-column-full govuk-!-margin-top-6">Platform metrics</h1>
-
+<main class="govuk-main-wrapper">
 <div class="govuk-width-container">
-  <main class="govuk-main-wrapper">
+
     <div class="govuk-grid-row">
-      <div class="govuk-grid-column-two-thirds">
+      <div class="govuk-grid-column-one-third">
       <h2 class="gem-c-heading gem-c-heading--font-size-27">Sites hosted on the platform</h2>
         <span class="govuk-heading-xl"><?php echo get_site_count(); ?></span>
       </div>
+      <div class="govuk-grid-column-one-third">
+      <h2 class="gem-c-heading gem-c-heading--font-size-27">Site monitoring and performance</h2>
+        <a href="https://github.com/ministryofjustice/hale-platform/actions/workflows/cd.yaml">
+        <img src="https://github.com/ministryofjustice/hale-platform/actions/workflows/cd.yaml/badge.svg?branch=main" alt="Hale Platform Deployment">
+        </a>
+        <ul class="govuk-list govuk-!-margin-bottom-9">
+        <li>
+        <a href="https://github.com/ministryofjustice/hale-platform">Hale Platform GitHub repository</a>
+        </li>
+            <li>
+        <a href="https://grafana.live.cloud-platform.service.justice.gov.uk/d/85a562078cdf77779eaa1add43ccec1e/kubernetes-compute-resources-namespace-pods?orgId=1&refresh=10s&var-datasource=default&var-cluster=&var-namespace=hale-platform-prod">Grafana dashboard (prod)</a>
+        </li>
+        <li> 
+        <a href="https://logs.cloud-platform.service.justice.gov.uk/_dashboards/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(columns:!(_source),filters:!(),index:b95d8900-dd15-11ed-87c8-170407f57c9c,interval:auto,query:(language:kuery,query:''),sort:!())">Ingress logs</a>
+    </li> 
+    </ul>    
     </div>
-  </main>
+    <div class="govuk-grid-column-one-third">
+      <h2 class="gem-c-heading gem-c-heading--font-size-27">Next site to go live:</h2>
+      <p> Prisons and Probation Ombudsman (PPO) - https://www.ppo.gov.uk/</p>
+</div>
+    </div>
+
 </div>
 
 <div class="govuk-grid-column-full">
@@ -82,6 +103,7 @@ foreach ( $sites as $site ) {
 }
 ?>
 </div>
+</main>
 <?php
 get_footer();
 
